af93e5b049bd98ad84f5c7c2875f247b407ef51c
Kailas Moon <kailasmoon@gmail.com>
2018-09-04 03:46:32 -0400

    Minor bug fix

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(__dirname ));
+
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
 });
